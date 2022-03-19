dd1650de29d63e7f746f76a50e9a01d6bed7c535
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2022-03-19 19:02:24 -0300

    feat: get results from internal api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,49 @@
 import { useState } from "react";
 import type { NextPage } from "next";
 
+import { getCoordinates } from "../src/services/getForecast";
+
+type Coordinates = {
+  latitude?: number;
+  longitude?: number;
+};
+
 const Home: NextPage = () => {
-  const [address, setAddress] = useState("");
+  const [address, setAddress] = useState(
+    "4600 Silver Hill Rd, Washington, DC 20233"
+  );
+  const [coordinates, setCoordinates] = useState<Coordinates>({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAddress(e.target.value);
   };
 
-  const getForecast = () => {};
+  const handleClick = async () => {
+    if (!address) return;
+    setIsLoading(true);
+
+    const coordinatesResult = await getCoordinates(address);
+    setCoordinates(coordinatesResult);
+
+    setIsLoading(false);
+  };
 
   return (
     <div>
       <h1>Weather US National</h1>
       <input type="text" onChange={handleAddress} value={address} />
-      <button onClick={getForecast}>Get forecast</button>
+      <button onClick={handleClick} disabled={!address || isLoading}>
+        Get forecast
+      </button>
+
+      {/* Using && but it could be a ternary when needed (avoid render 0 in some cases) */}
+      {coordinates.latitude && coordinates.longitude && (
+        <>
+          <p>Latitude (x): {coordinates.latitude}</p>
+          <p>Longitude (y): {coordinates.longitude}</p>
+        </>
+      )}
     </div>
   );
 };
--- src/services/getForecast.tsx
+++ src/services/getForecast.tsx
@@ -1,16 +1,29 @@
 /**
- * TODO:
- * 1. get coordinates - Structure number and street name (required)
- * https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=4600+Silver+Hill+Rd%2C+Washington%2C+DC+20233&benchmark=2020&format=json
- * 2. get forecast - https://api.weather.gov/points/{latitude},{longitude}
+ * Call an internal serverless function (avoid CORS error) to get the coordinates.
+ * @param {String} address e.g: 4600 Silver Hill Rd, Washington, DC 20233
+ * @returns {Object} e.g: { latitude: -76.92744, longitude: 38.845985 }
  */
+export const getCoordinates = async (address: string) => {
+  const config = {
+    method: "POST",
+    body: JSON.stringify({ address }),
+  };
 
-const getCoordinates = async () => {
   try {
+    const data = await fetch("/api/get-coordinates", config)
+      .then((res) => res.json())
+      .then((data) => data);
+
+    return data;
   } catch (error) {
     console.log(error);
   }
 };
+
+/**
+ * TODO:
+ * get forecast - https://api.weather.gov/points/{latitude},{longitude}
+ */
 
 export const getForecast = async () => {
   try {
