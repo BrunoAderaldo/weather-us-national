be80baeb49f55f38726ae67b1404b3e977d17f95
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2022-03-19 15:24:59 -0300

    chore: initial commit

---
+++ src/services/getForecast.tsx
@@ -0,0 +1,20 @@
+/**
+ * TODO:
+ * 1. get coordinates - Structure number and street name (required)
+ * https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=4600+Silver+Hill+Rd%2C+Washington%2C+DC+20233&benchmark=2020&format=json
+ * 2. get forecast - https://api.weather.gov/points/{latitude},{longitude}
+ */
+
+const getCoordinates = async () => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getForecast = async () => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
